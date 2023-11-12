42a16f966fbeae1213c27dbfbf6929620f6294ef
N1ckDunn <nick.dunn@nccgroup.com>
2023-11-12 16:12:13 +0000

    Add files via upload
    Modifications to cope with different JSON formats

--- dentist.py
+++ dentist.py
@@ -2,7 +2,7 @@
 
 # An extractor script for/from John & Dave's Data-Bouncing project:	https://thecontractor.io/data-bouncing/
 # This script will extract JSON data and write to an output file for the bouncer. If you dont know what you're doing, go read the posts :) 
-# Have fun, dont be a dick... Seriously... don't. 
+# Have fun, dont do anything illegal... Seriously... don't. 
 
 import json
 import re
@@ -25,23 +25,47 @@ string_to_remove = sys.argv[2]
 # Array to hold final output
 output_lines = []
 data = []
+targets = []
 
 # Process the JSON file and write the output to output file
 with open(json_file, 'r') as input_file:
+
     for json_obj in input_file:
-        data = json.loads(json_obj)
+
+		# Process JSON data
+        try:
+            json_content = json.loads(json_obj)
+
+            # We do this as interactsh server renders all JSON content inside the app attribute, containing the data in a 'data' attribute
+            if "app" in json_content:
+                temp_data = json_content["app"]
+                json_data  = json.loads(temp_data)
+                for data in json_data["data"]:
+                    id_chars = data["full-id"]
+                    targets += ["" . join([str(ch) for ch in id_chars])]
+
+            else:
+                id_chars = json_content["full-id"]
+                targets += ["" . join([str(ch) for ch in id_chars])]
+                #targets += json_content.get("full-id", [])
+        except:
+            print("Error reading and parsing data from JSON file.")
 
         # Extract data
-        target = data.get("full-id", [])
-        parts = target.split(".")[0:3]
+        
 
-        # Remove specified string
-        joined_string = '.'.join(parts)
-        lowercase_string = joined_string.lower()
-        processed_string = lowercase_string.replace(string_to_remove, "")
-    
-        if re.match('^(host\.|xff\.|ref\.|wafp\.|cfcon\.|root@\.|rip\.|trip\.|xclip\.|ff\.|origip\.|clip\.)', processed_string):
-            output_lines.append(processed_string)
+        for target in targets:
+            print(target)
+            parts = target.split(".")
+            parts.pop()
+
+            # Remove specified string
+            joined_string = '.'.join(parts)
+            lowercase_string = joined_string.lower()
+            #processed_string = lowercase_string.replace(string_to_remove, "")
+        
+            if re.match('^(host\.|xff\.|ref\.|wafp\.|cfcon\.|root@\.|rip\.|trip\.|xclip\.|ff\.|origip\.|clip\.)', lowercase_string):
+                output_lines.append(lowercase_string)
 
 # Sort results and write to output file
 output_lines.sort()
